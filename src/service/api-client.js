17585b1dd19e777c68270b02e9bcedae9d98c5c2
Etika <etika8445@gmail.com>
2024-08-17 20:36:35 +0530

    Update api-client

--- src/service/api-client.js
+++ src/service/api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 async function getdata({cityname}){  //fetching data arounding to cityname
     // console.log(cityname);
-    const URL=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=dc344b58cab262d0b9a1628081caf913`;
+    const URL=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${API_KEY}`;
     const response= await axios.get(URL);
     // console.log(response.data);
     return response.data;
